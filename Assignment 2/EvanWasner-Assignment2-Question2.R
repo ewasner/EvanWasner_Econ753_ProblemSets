6d4da74fba48058a9df60c20b8458ed34e1dc8af
ewasner <ewasner@umass.edu>
2020-10-13 01:59:16 -0400

    Continuing work and logging progress... Almost done...

--- Assignment 2/EvanWasner-Assignment2-Question2.R
+++ Assignment 2/EvanWasner-Assignment2-Question2.R
@@ -13,6 +13,10 @@ library(XLConnect)
 rm(list = ls())
 dev.off()
 
+#####################################################
+##            Inflation calculations               ##
+#####################################################
+
 ## Read CPI-U data from spreadsheet
 cpiU <- readWorksheetFromFile("CPI_U.xls",
                               sheet="Monthly",
@@ -33,10 +37,23 @@ RcpiURS <- readWorksheetFromFile("R_CPI_U_RS.xlsx",
                                  startRow=6,
                                  startCol=1)
 
+cpiUX1 <- readWorksheetFromFile("ERP-2012-table62.xls",
+                                sheet="B62",
+                                startRow=5,
+                                startCol=1,
+                                endRow=15,
+                                endCol=10,
+                                header=FALSE)
+cpiUX1 <- data.frame(year=cpiUX1[,1],cpi=cpiUX1[,10])
+
+## CPI-U-X1 rate of change
+cpiUX1 <- mutate(cpiUX1, change=(cpi-lead(cpi))/lead(cpi))
+
 ## Separate year and month
 cpiU <- mutate(cpiU, year=str_split_fixed(date, "-", 3)[,1], month=str_split_fixed(date, "-", 3)[,2])
 CcpiU <- mutate(CcpiU, year=str_split_fixed(date, "-", 3)[,1], 
                 month=str_split_fixed(date, "-", 3)[,2])
+cpiUX1 <- mutate(cpiUX1, year=substr(cpiUX1$year,1,4))
 
 
 
@@ -55,6 +72,19 @@ CcpiUAnnual <- mutate(CcpiUAnnual, cpi = cpi/(0.01*CcpiUBase))
 
 RcpiURSAnnual <- data.frame(year=as.character(RcpiURS$YEAR), cpi=RcpiURS$AVG)
 RcpiURSAnnual <- cbind(RcpiURSAnnual, type="R-CPI-U-RS")
+
+## Calculate CPI-U-RS values for 1968-1977 based on change in CPI-U-X1
+cpiUX1 <- cbind(cpiUX1, cpiURS=vector(mode="double", length=11))
+cpiUX1[11,4] <- as.double(filter(RcpiURSAnnual,year==1978)$cpi)
+for (val in 10:1){
+  cpiUX1[val,4] <- cpiUX1[val+1,4]*(1+cpiUX1[val,3])
+}
+
+## Input extrapolated years in R-CPI-U-RS
+RcpiURSAnnual <- filter(RcpiURSAnnual, year>1978)
+RcpiURSAnnual <- rbind(data.frame(year=cpiUX1$year,cpi=cpiUX1$cpiURS, type="R-CPI-U-RS"), RcpiURSAnnual)
+
+## Convert R-CPI-U-RS to 2018 base
 RcpiURSBase <- as.double(filter(RcpiURSAnnual, year==2018)[2][1])
 RcpiURSAnnual <- mutate(RcpiURSAnnual, cpi = cpi/(0.01*RcpiURSBase))
 
@@ -63,11 +93,31 @@ cpiUAnnual <- mutate(cpiUAnnual, previous=lag(cpi), inflation=100*(cpi-previous)
 CcpiUAnnual <- mutate(CcpiUAnnual, previous=lag(cpi), inflation=100*(cpi-previous)/previous)
 RcpiURSAnnual <- mutate(RcpiURSAnnual, previous=lag(cpi), inflation=100*(cpi-previous)/previous)
 
+## Save data
+save(cpiUAnnual,file="cpiUAnnual.Rdata")
+save(CcpiUAnnual,file="CcpiUAnnual.Rdata")
+save(RcpiURSAnnual,file="RcpiURSAnnual.Rdata")
 
-ggplot(filter(bind_rows(cpiUAnnual,CcpiUAnnual,RcpiURSAnnual), year >= 1970), aes(x=year)) + 
-  geom_line(aes(y=inflation, color=type, group = 1), size=2) +
-  scale_x_discrete(breaks=c(1970,1980,1990,2000,2010,2020))
+# ggplot(filter(bind_rows(cpiUAnnual,CcpiUAnnual,RcpiURSAnnual), year >= 1970), aes(x=year)) + 
+#   geom_line(aes(y=inflation, color=type, group = 1), size=2) +
+#   scale_x_discrete(breaks=c(1970,1980,1990,2000,2010,2020))
 
-ggplot(filter(bind_rows(cpiUAnnual,CcpiUAnnual,RcpiURSAnnual), year >= 1970), aes(x=year)) + 
+inflationPlot <- ggplot(filter(bind_rows(cpiUAnnual,CcpiUAnnual,RcpiURSAnnual), 
+                               year >= 1970), aes(x=year)) + 
   geom_point(aes(y=inflation, color=type), size=2) +
   scale_x_discrete(breaks=c(1970,1980,1990,2000,2010,2020))
+save(inflationPlot,file="inflationPlot.Rdata")
+
+
+#####################################################
+##                Minimum Wage                     ##
+#####################################################
+
+nominalWage <- readWorksheetFromFile("Min_wage.xlsx",
+                              sheet="Min wageHistory of Federal Mini",
+                              startRow=6,
+                              startCol=1,
+                              endCol=2)
+
+
+
